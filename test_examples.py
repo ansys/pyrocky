71b3e5b657a70048f4383161a6c036d45e07f695
Gustavo Martins <gustavo.martins@ansys.com>
2023-12-04 16:41:54 -0300

    Fix to avoid problems on Sphinx doc generation
    ROCKY-20651

--- test_examples.py
+++ test_examples.py
@@ -4,10 +4,9 @@ import sys
 
 import pytest
 
-EXAMPLES_DIR = Path(__file__).parent / "basic_examples"
-EXAMPLE_FILES = EXAMPLES_DIR
+EXAMPLES_DIR = Path(__file__).parent / "examples"
 
 
-@pytest.mark.parametrize("example_path", list(EXAMPLE_FILES.glob("*.py")))
+@pytest.mark.parametrize("example_path", list(EXAMPLES_DIR.glob("*.py")))
 def test_examples(example_path: Path) -> None:
     subprocess.check_call([sys.executable, str(example_path.absolute())])

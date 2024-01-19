4c8a06a1cc576284c30b31a10ce8c0e4b11f7e26
afernand <alex.fernandezluces@ansys.com>
2024-01-19 09:58:05 +0100

    fix comments

--- src/ansys/rocky/core/exceptions.py
+++ src/ansys/rocky/core/exceptions.py
@@ -22,14 +22,12 @@
 
 
 class PyRockyError(Exception):
-    """Generic exception for PyRocky API"""
+    """Generic exception for PyRocky API."""
 
 
 class RockyLaunchError(PyRockyError):
-    """Raised for errors occurred during Rocky application launch"""
+    """Raised for errors occurred during Rocky application launch."""
 
 
 class RockyApiError(Exception):
-    """
-    Exception class representing an error generated in the API layer.
-    """
+    """Exception class representing an error generated in the API layer."""

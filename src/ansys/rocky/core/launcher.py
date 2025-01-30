5c0b0b53cfd7e32bbc9d04b9b6d016ec7d9c057c
Adrian Brandemarte Moreira <adrian.brandemartemoreira@ansys.com>
2025-01-30 14:04:26 -0300

    Change rocky bin path

--- src/ansys/rocky/core/launcher.py
+++ src/ansys/rocky/core/launcher.py
@@ -326,7 +326,7 @@ def _get_exec_using_tools_path(  # pragma: no cover
         for installation in sorted(ansys_installations, reverse=True):
             executable = (
                 Path(ansys_installations[installation])
-                / f"{product_name.lower()}/bin/{product_name}"
+                / f"{product_name.lower()}/{product_name}"
             )
             if executable.is_file() and installation >= MINIMUM_ANSYS_VERSION_SUPPORTED:
                 break
@@ -339,7 +339,7 @@ def _get_exec_using_tools_path(  # pragma: no cover
             raise FileNotFoundError(f"Local executable is not found.")
 
         executable = (
-            Path(ansys_installation) / f"{product_name.lower()}/bin/{product_name}"
+            Path(ansys_installation) / f"{product_name.lower()}/{product_name}"
         )
 
     return executable

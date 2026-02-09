dafd8e5989f24bc9791810aeb967ac2116c9d44a
Felipe Marchi <fepmarchi@gmail.com>
2026-02-09 15:26:48 -0300

    feat: Support for launching a rocky and freeflow docker containers

--- tests/test_container.py
+++ tests/test_container.py
@@ -19,20 +19,35 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""PyRocky is a Python client for Ansys Rocky application."""
-import importlib.metadata as importlib_metadata
 
-from .client import connect, connect_to_rocky
-from .launcher import launch_container, launch_freeflow, launch_rocky
+import os
+import sys
 
-__version__ = importlib_metadata.version(__name__.replace(".", "-"))
-"""PyRocky version."""
+import pytest
+
+import ansys.rocky.core as pyrocky
+
+VERSION = int(os.getenv("ANSYS_VERSION", "261"))
+RUN_DOCKER_TESTS = int(os.getenv("RUN_DOCKER_TESTS", "0"))
+
+if not RUN_DOCKER_TESTS or VERSION != 261 or sys.platform == "win32":
+    pytest.skip(
+        "Docker tests must be explicitly enabled by setting RUN_DOCKER_TESTS=1"
+        " and only supports Ansys version 26.1.0 in linux for now.",
+        allow_module_level=True,
+    )
 
 
-__all__ = [
-    "connect_to_rocky",
-    "connect",
-    "launch_rocky",
-    "launch_freeflow",
-    "launch_container",
-]
+@pytest.mark.parametrize("variant", ["freeflow", "rocky"])
+def test_launch_container(variant, request):
+    """
+    Minimal test to check if it is possible to launch a Rocky and Freeflow container
+    and communicate with it.
+    """
+    client = pyrocky.launch_container(variant)
+    request.addfinalizer(client.close)
+
+    project = client.api.CreateProject()
+
+    study = project.GetStudy()
+    assert study is not None
